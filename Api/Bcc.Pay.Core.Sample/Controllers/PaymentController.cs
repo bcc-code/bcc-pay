97879e40bd8b71cfd1d5eef41cf1a8d2d5fbdfc2
markiyanshvets <markiyan.shvets@gmail.com>
2021-07-20 12:00:04 +0300

    Sample project, gitactions, ravenDB extensions;

---
+++ Api/Bcc.Pay.Core.Sample/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bcc.Pay.Core.Sample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        [HttpGet("test")]
+        public IActionResult Test() => Ok(new { TestConnection = true });
+    }
+}
