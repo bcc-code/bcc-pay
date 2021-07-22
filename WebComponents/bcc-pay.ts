287ef7b33bcefa1ca2bb0f5773552a5a07a6bb1c
Oskar Poprawski <oskar.poprawski@bcc.no>
2021-07-22 09:41:06 +0200

    WebComponent with open-wc added

--- WebComponents/bcc-pay.ts
+++
@@ -1,7 +0,0 @@
-class BccPay extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `<h1>Hello from BCC PAY</h1>`;
-  }
-}
-
-customElements.define("bcc-pay", BccPay);
---
+++ WebComponents/bcc-pay/bcc-pay.ts
@@ -0,0 +1,3 @@
+import { BccPay } from './src/BccPay.js';
+
+window.customElements.define('bcc-pay', BccPay);
