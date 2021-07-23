69d0b0dd1665274fa5efa008c161676c3b882299
Oskar Poprawski <oskar.poprawski@bcc.no>
2021-07-23 12:21:55 +0200

    Sample using of bcc-pay in anglar added

--- UI/dashboard/src/app/app.module.ts
+++ UI/dashboard/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopBarComponent } from './shared/components/top-bar/top-bar.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
   declarations: [AppComponent, TopBarComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
