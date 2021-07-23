c222204e60873e02dc3a56316f08336b64d24b53
Oskar Poprawski <oskar.poprawski@bcc.no>
2021-07-23 10:18:37 +0200

    Publish npm package corrections

--- WebComponents/bcc-pay/bcc-pay.ts
+++ WebComponents/bcc-pay/bcc-pay.ts
@@ -1,3 +1,9 @@
 import { BccPay } from './src/BccPay.js';
 
 window.customElements.define('bcc-pay', BccPay);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'bcc-pay': BccPay;
+  }
+}
