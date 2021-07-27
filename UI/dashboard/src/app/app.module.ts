2ce8d60632ac73059a6bf704c65d3ed4c6273357
markiyanshvets <markiyan.shvets@gmail.com>
2021-07-27 19:08:55 +0300

    merge main to payment-providers

---
+++ UI/dashboard/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopBarComponent } from './shared/components/top-bar/top-bar.component';
+
+@NgModule({
+  declarations: [AppComponent, TopBarComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ UI/dashboard/src/app/modules/transactions-list/transactions-list.component.ts
@@ -0,0 +1,85 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+export interface Transaction {
+  id: string;
+  email: string;
+  amount: number;
+  currency: string;
+  country: string;
+}
+
+const TRANSACTIONS_DATA: Transaction[] = [
+  {
+    id: '0aaa4d10',
+    email: 'test0@test.no',
+    amount: 50,
+    currency: 'NOK',
+    country: 'NO',
+  },
+  {
+    id: '52e837dbc8ad',
+    email: 'test@test.no',
+    amount: 40,
+    currency: 'NOK',
+    country: 'NO',
+  },
+  {
+    id: '6baa4d10',
+    email: 't@test.no',
+    amount: 55,
+    currency: 'NOK',
+    country: 'NO',
+  },
+  {
+    id: '5be837dbc8ad',
+    email: 'test1@test.no',
+    amount: 1,
+    currency: 'BTC',
+    country: 'ES',
+  },
+  {
+    id: '3aaa4d10',
+    email: 'test@test.no',
+    amount: 30,
+    currency: 'NOK',
+    country: 'NO',
+  },
+  {
+    id: '5ce837dbc8ad',
+    email: 'test2@test.no',
+    amount: 10,
+    currency: 'USD',
+    country: 'US',
+  },
+  {
+    id: '6faa4d10',
+    email: 'test@test.no',
+    amount: 60,
+    currency: 'NOK',
+    country: 'NO',
+  },
+];
+
+@Component({
+  selector: 'app-transactions-list',
+  templateUrl: './transactions-list.component.html',
+  styleUrls: ['./transactions-list.component.scss'],
+})
+export class TransactionsListComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'email', 'amount', 'currency', 'country'];
+  dataSource = new MatTableDataSource<Transaction>(TRANSACTIONS_DATA);
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  public applyFilter = (value: string) => {
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+  };
+}
