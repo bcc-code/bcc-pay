d04c97c0db0b57f947c589cc106d34c16f6c59ef
markiyanshvets <markiyan.shvets@gmail.com>
2021-08-02 14:30:40 +0300

    added empty ctor; relocate classes in commands

--- Api/BccPay.Core.Cqrs/Commands/CreatePaymentCommand.cs
+++ Api/BccPay.Core.Cqrs/Commands/CreatePaymentCommand.cs
@@ -11,6 +11,10 @@ namespace BccPay.Core.Cqrs.Commands
 {
     public class CreatePaymentCommand : IRequest<string>
     {
+        public CreatePaymentCommand()
+        {
+        }
+
         public CreatePaymentCommand(string payerId,
             string currencyCode,
             decimal amount,
@@ -26,48 +30,48 @@ namespace BccPay.Core.Cqrs.Commands
         public string CurrencyCode { get; set; }
         public string CountryCode { get; set; }
         public decimal Amount { get; set; }
+    }
 
-        public class CreatePaymentCommandValidator : AbstractValidator<CreatePaymentCommand>
+    public class CreatePaymentCommandValidator : AbstractValidator<CreatePaymentCommand>
+    {
+        public CreatePaymentCommandValidator()
         {
-            public CreatePaymentCommandValidator()
-            {
-                RuleFor(x => x.CurrencyCode)
-                    .Matches(new Regex(@"^([A-Z]{3})$"))
-                    .NotEmpty()
-                    .WithMessage("Invalid currency code");
+            RuleFor(x => x.CurrencyCode)
+                .Matches(new Regex(@"^([A-Z]{3})$"))
+                .NotEmpty()
+                .WithMessage("Invalid currency code");
 
-                RuleFor(x => x.CountryCode)
-                    .MinimumLength(2)
-                    .MaximumLength(3)
-                    .WithMessage("Invalid country code, use alpha2, alpha3 or numeric codes");
+            RuleFor(x => x.CountryCode)
+                .MinimumLength(2)
+                .MaximumLength(3)
+                .WithMessage("Invalid country code, use alpha2, alpha3 or numeric codes");
 
-                RuleFor(x => x.Amount)
-                    .GreaterThan(0)
-                    .WithMessage("Invalid amount, must be greater than 0");
-            }
+            RuleFor(x => x.Amount)
+                .GreaterThan(0)
+                .WithMessage("Invalid amount, must be greater than 0");
+        }
+    }
+
+    public class CreatePaymentCommandHandler : IRequestHandler<CreatePaymentCommand, string>
+    {
+        private readonly IAsyncDocumentSession _documentSession;
+
+        public CreatePaymentCommandHandler(IAsyncDocumentSession documentSession)
+        {
+            _documentSession = documentSession
+                ?? throw new ArgumentNullException(nameof(documentSession));
         }
 
-        public class CreatePaymentCommandHandler : IRequestHandler<CreatePaymentCommand, string>
+        public async Task<string> Handle(CreatePaymentCommand request, CancellationToken cancellationToken)
         {
-            private readonly IAsyncDocumentSession _documentSession;
+            var payment = new Payment();
 
-            public CreatePaymentCommandHandler(IAsyncDocumentSession documentSession)
-            {
-                _documentSession = documentSession
-                    ?? throw new ArgumentNullException(nameof(documentSession));
-            }
+            payment.Create(request.PayerId, request.CurrencyCode, request.CountryCode, request.Amount);
 
-            public async Task<string> Handle(CreatePaymentCommand request, CancellationToken cancellationToken)
-            {
-                var payment = new Payment();
+            await _documentSession.StoreAsync(payment, cancellationToken);
+            await _documentSession.SaveChangesAsync(cancellationToken);
 
-                payment.Create(request.PayerId, request.CurrencyCode, request.CountryCode, request.Amount);
-
-                await _documentSession.StoreAsync(payment, cancellationToken);
-                await _documentSession.SaveChangesAsync(cancellationToken);
-
-                return payment.PaymentId.ToString();
-            }
+            return payment.PaymentId.ToString();
         }
     }
 }
