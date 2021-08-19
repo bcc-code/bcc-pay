935fe582e12c87220d740ace60bbbc47b2953d30
markiyanshvets <markiyan.shvets@gmail.com>
2021-08-19 15:30:17 +0300

    Query to get problematic payments

---
+++ Api/BccPay.Core.Cqrs/Queries/GetProblematicPaymentsQuery.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using BccPay.Core.DataAccess.Indexes;
+using MediatR;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+
+namespace BccPay.Core.Cqrs.Queries
+{
+    public class GetProblematicPaymentsQuery : IRequest<ProblematicPaymentsResult>
+    {
+    }
+
+    public record ProblematicPaymentsResult(List<ProblematicPaymentsIndex.Result> Payments);
+
+    public class GetProblematicPaymentsQueryHandler : IRequestHandler<GetProblematicPaymentsQuery, ProblematicPaymentsResult>
+    {
+        private readonly IAsyncDocumentSession _documentSession;
+
+        public GetProblematicPaymentsQueryHandler(IAsyncDocumentSession documentSession)
+        {
+            _documentSession = documentSession;
+        }
+
+        public async Task<ProblematicPaymentsResult> Handle(GetProblematicPaymentsQuery request, CancellationToken cancellationToken)
+        {
+            var queryResult = await _documentSession
+                .Query<ProblematicPaymentsIndex.Result, ProblematicPaymentsIndex>()
+                .ToListAsync(token: cancellationToken);
+
+            return new ProblematicPaymentsResult(queryResult);
+        }
+    }
+}
---
+++ Api/BccPay.Core.DataAccess/Indexes/ProblematicPaymentsIndex.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BccPay.Core.Domain.Entities;
+using BccPay.Core.Enums;
+using Raven.Client.Documents.Indexes;
+
+namespace BccPay.Core.DataAccess.Indexes
+{
+    public class ProblematicPaymentsIndex : AbstractMultiMapIndexCreationTask<ProblematicPaymentsIndex.Result>
+    {
+        public ProblematicPaymentsIndex()
+        {
+            AddMap<Payment>(payments => from payment in payments
+                                        where payment.Attempts
+                                            .Where(x => x.AttemptStatus == Enums.AttemptStatus.PaymentIsSuccessful).Count() > 1
+                                        select new Result
+                                        {
+                                            PaymentId = payment.PaymentId,
+                                            PayerId = payment.PayerId,
+                                            Created = payment.Created,
+                                            Updated = payment.Updated,
+                                            Description = payment.Description,
+                                            CountryCode = payment.CountryCode,
+                                            CurrencyCode = payment.CurrencyCode,
+                                            Amount = payment.Amount,
+                                            Attempts = payment.Attempts.Select(x => new AttemptResult
+                                            {
+                                                AttemptStatus = x.AttemptStatus,
+                                                CountryCode = x.CountryCode,
+                                                Created = x.Created,
+                                                PaymentMethod = x.PaymentMethod,
+                                                PaymentProvider = x.PaymentProvider
+                                            }).ToList(),
+                                        });
+
+            Store(x => x.Attempts, FieldStorage.Yes);
+        }
+
+        public class Result
+        {
+            public string CurrencyCode { get; set; }
+            public Guid PaymentId { get; set; }
+            public string PayerId { get; set; }
+            public DateTime Created { get; set; }
+            public DateTime? Updated { get; set; }
+            public string Description { get; set; }
+            public string CountryCode { get; set; }
+            public decimal Amount { get; set; }
+            public List<AttemptResult> Attempts { get; set; }
+        }
+
+        public class AttemptResult
+        {
+            public PaymentMethod PaymentMethod { get; set; }
+            public PaymentProvider PaymentProvider { get; set; }
+            public AttemptStatus AttemptStatus { get; set; }
+            public string CountryCode { get; set; }
+            public DateTime Created { get; set; }
+        }
+    }
+}
