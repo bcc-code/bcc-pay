e6e1c37bc1ab6ee631c337d96f40f7f887b96a47
markiyanshvets <markiyan.shvets@gmail.com>
2021-09-02 13:04:39 +0300

    configuration helper, exception added

---
+++ Api/BccPay.Core.Infrastructure/Exceptions/NullConfigurationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+namespace BccPay.Core.Infrastructure.Exceptions
+{
+    public class NullConfigurationException : Exception
+    {
+        public NullConfigurationException(string message = "Configuration must be set upped", HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+
+        public HttpStatusCode StatusCode { get; private set; }
+    }
+}
