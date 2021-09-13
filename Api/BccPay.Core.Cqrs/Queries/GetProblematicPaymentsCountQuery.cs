f25a198f952508d45e0f8f6928f1c40512f66624
Markiyan <34346633+markiyanshvets@users.noreply.github.com>
2021-09-13 11:13:52 +0300

    added (#60)

---
+++ Api/BccPay.Core.Cqrs/Queries/GetProblematicPaymentsCountQuery.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BccPay.Core.Domain.Entities;
+using MediatR;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Linq;
+using Raven.Client.Documents.Session;
+
+namespace BccPay.Core.Cqrs.Queries
+{
+    public class GetProblematicPaymentsCountQuery : IRequest<ProblematicPaymentsCountResult>
+    {
+    }
+
+    public record ProblematicPaymentsCountResult(int Count);
+
+    public class GetProblematicPaymentsCountQueryHandler : IRequestHandler<GetProblematicPaymentsCountQuery, ProblematicPaymentsCountResult>
+    {
+        private readonly IAsyncDocumentSession _documentSession;
+
+        public GetProblematicPaymentsCountQueryHandler(IAsyncDocumentSession documentSession)
+        {
+            _documentSession = documentSession;
+
+        }
+
+        public async Task<ProblematicPaymentsCountResult> Handle(GetProblematicPaymentsCountQuery request, CancellationToken cancellationToken)
+        {
+            var problematicPaymentsCount = await _documentSession
+                .Query<Payment>()
+                .Where(payment => payment.IsProblematic)
+                .CountAsync(token: cancellationToken);
+
+            return new ProblematicPaymentsCountResult(problematicPaymentsCount);
+        }
+    }
+}
