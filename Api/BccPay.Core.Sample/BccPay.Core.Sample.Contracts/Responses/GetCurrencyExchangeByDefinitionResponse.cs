6b7b537ef63bbf796dbc6db0453cc6f3a2c6f19e
Markiyan <34346633+markiyanshvets@users.noreply.github.com>
2021-10-26 10:51:58 +0300

    Enhancement/exchange samvirk (#81)
    * exchange for samvirk
    
    * default value for fromCurrency
    
    * changes
    
    * typo
    
    * changes
    
    * remove unused
    
    * changes
    
    * removed to

---
+++ Api/BccPay.Core.Sample/BccPay.Core.Sample.Contracts/Responses/GetCurrencyExchangeByDefinitionResponse.cs
@@ -0,0 +1,9 @@
+﻿using BccPay.Core.Enums;
+
+namespace BccPay.Core.Sample.Contracts.Responses
+{
+    public record GetCurrencyExchangeByDefinitionResponse(
+        Currencies FromCurrency,
+        Currencies ToCurrency,
+        decimal ExchangeRate);
+}
