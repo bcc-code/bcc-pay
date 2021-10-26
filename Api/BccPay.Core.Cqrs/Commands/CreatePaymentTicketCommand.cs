eadd731f0ce73006d5ed271a1ef24988fc5d0d48
markiyanshvets <markiyan.shvets@gmail.com>
2021-10-26 10:51:41 +0300

    tickets init

---
+++ Api/BccPay.Core.Cqrs/Commands/CreatePaymentTicketCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BccPay.Core.Domain;
+using BccPay.Core.Domain.Entities;
+using BccPay.Core.Enums;
+using MediatR;
+using Raven.Client.Documents.Session;
+
+namespace BccPay.Core.Cqrs.Commands
+{
+    public class CreatePaymentTicketCommand : IRequest
+    {
+        public string PaymentDefinitionId { get; set; }
+        public Currencies BaseCurrency { get; set; }
+        public Currencies DestinationCurrency { get; set; }
+        public string PayerId { get; set; }
+        public decimal SourceCurrencyAmount { get; set; }
+    }
+
+    public class CreatePaymentTicketCommandHandler : IRequestHandler<CreatePaymentTicketCommand>
+    {
+        private readonly IAsyncDocumentSession _documentSession;
+        private readonly IMediator _mediator;
+        
+        public CreatePaymentTicketCommandHandler(IAsyncDocumentSession documentSession, IMediator mediator)
+        {
+            _documentSession = documentSession
+                               ?? throw new ArgumentNullException(nameof(documentSession));
+            _mediator = mediator;
+        }
+
+        public async Task<Unit> Handle(CreatePaymentTicketCommand request, CancellationToken cancellationToken)
+        {
+            var definition = await _documentSession.LoadAsync<PaymentProviderDefinition>(
+                PaymentProviderDefinition.GetDocumentId(request.PaymentDefinitionId), cancellationToken);
+            
+            
+            
+           // _documentSession.StoreAsync<PaymentTicket>(new PaymentTicket());
+            return new Unit();
+        }
+    }
+}
---
+++ Api/BccPay.Core.Domain/Entities/PaymentTicket.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using BccPay.Core.Enums;
+
+namespace BccPay.Core.Domain.Entities
+{
+    public class PaymentTicket
+    {
+        public static string GetDocumentId(Guid ticketId)
+            => $"paymentTickets/{ticketId}";
+
+        public string Id => GetDocumentId(TicketId);
+
+        public Guid TicketId { get; set; }
+
+        public string PaymentDefinitionId { get; set; }
+        public string PayerId { get; set; }
+
+        public Currencies BaseCurrency { get; set; }
+        public Currencies DefinitionCurrency { get; set; }
+
+        public decimal SourceCurrencyAmount { get; set; }
+        public decimal DestinationCurrencyAmount { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+
+        public void Create(Currencies baseCurrency,
+            Currencies definitionCurrency,
+            string paymentDefinitionId,
+            string payerId,
+            decimal sourceCurrencyAmount,
+            decimal destinationCurrencyAmount)
+        {
+            TicketId = Guid.NewGuid();
+            Created = DateTime.UtcNow;
+            PayerId = payerId;
+
+            PaymentDefinitionId = paymentDefinitionId;
+            BaseCurrency = baseCurrency;
+            DefinitionCurrency = definitionCurrency;
+            SourceCurrencyAmount = sourceCurrencyAmount;
+            DestinationCurrencyAmount = destinationCurrencyAmount;
+        }
+
+        public void Update(Currencies baseCurrency,
+            Currencies definitionCurrency,
+            string paymentDefinitionId,
+            decimal sourceCurrencyAmount,
+            decimal destinationCurrencyAmount)
+        {
+            Updated = DateTime.UtcNow;
+
+            PaymentDefinitionId = paymentDefinitionId;
+            BaseCurrency = baseCurrency;
+            DefinitionCurrency = definitionCurrency;
+            SourceCurrencyAmount = sourceCurrencyAmount;
+            DestinationCurrencyAmount = destinationCurrencyAmount;
+        }
+    }
+}
