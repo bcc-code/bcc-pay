2cb3ee0569681b5bf9c0e06c8d4b773630db34a3
Markiyan <34346633+markiyanshvets@users.noreply.github.com>
2021-10-28 17:01:58 +0300

    fix amount (#87)

--- Api/BccPay.Core.Infrastructure/PaymentProviders/RequestBuilders/Implementations/MollieRequestBuilder.cs
+++ Api/BccPay.Core.Infrastructure/PaymentProviders/RequestBuilders/Implementations/MollieRequestBuilder.cs
@@ -4,31 +4,35 @@ using BccPay.Core.Infrastructure.BccPaymentSettings;
 using BccPay.Core.Infrastructure.Dtos;
 using BccPay.Core.Infrastructure.PaymentModels.MollieNodes;
 using BccPay.Core.Infrastructure.PaymentModels.Request.Mollie;
+using BccPay.Core.Shared.Converters;
 
 namespace BccPay.Core.Infrastructure.PaymentProviders.RequestBuilders.Implementations
 {
     internal class MollieRequestBuilder : IMollieRequestBuilder
     {
         private readonly MollieProviderOptions _options;
+
         public MollieRequestBuilder(MollieProviderOptions options)
         {
             _options = options;
         }
 
-        public MolliePaymentRequest BuildMolliePaymentRequest(PaymentRequestDto paymentRequest, PaymentMethod paymentMethod)
+        public MolliePaymentRequest BuildMolliePaymentRequest(PaymentRequestDto paymentRequest,
+            PaymentMethod paymentMethod)
         {
+            string normalizedAmount = String.Format("{0:0.00}", paymentRequest.Amount.TwoDigitsAfterPoint());
             return new MolliePaymentRequest
             {
                 Amount = new MollieAmount
                 {
                     Currency = Currencies.EUR.ToString(),
-                    Value = String.Format("{0:0.00}", paymentRequest.Amount)
+                    Value = normalizedAmount
                 },
                 Locale = GetLocale(paymentRequest.AcceptLanguage),
-                Method = new string[] { paymentMethod.ToString().ToLower() },
+                Method = new string[] {paymentMethod.ToString().ToLower()},
                 Description = paymentRequest.Description,
                 RedirectUrl = GetRedirectUrl(paymentRequest.IsMobile),
-                WebhookUrl = _options.WebhookUrl + $"/{ paymentRequest.PaymentId }",
+                WebhookUrl = _options.WebhookUrl + $"/{paymentRequest.PaymentId}",
                 Links = new { }
             };
         }
